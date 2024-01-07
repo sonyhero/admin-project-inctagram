d48d943d51768acabbbb819ce6b2523787133243
belozerov-egor <belaz.egor@gmail.com>
2024-01-08 01:42:48 +0300

    feat: add react-i18next

---
+++ src/shared/locales/en.ts
@@ -0,0 +1,12 @@
+export type LocaleType = typeof en
+
+export const en = {
+  header: {
+    admin: 'Admin',
+    en: 'English',
+    inctagram: 'Inctagram',
+    ru: 'Русский',
+    super: 'Super',
+  },
+  metaDescription: 'Admin panel for app Inсtagram ',
+}
---
+++ src/shared/locales/ru.ts
@@ -0,0 +1,11 @@
+import { LocaleType } from '@/shared/locales/en'
+export const ru: LocaleType = {
+  header: {
+    admin: 'Админ',
+    en: 'Английский',
+    inctagram: 'Инктаграм',
+    ru: 'Русский',
+    super: 'Супер',
+  },
+  metaDescription: 'Админ панель для приложения Инктаграм',
+}
