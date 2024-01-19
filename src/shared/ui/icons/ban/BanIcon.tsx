e3377e1bbbfc61f4ee5a1cd90b1237906a8e2128
sonyhero <27anton06@gmail.com>
2024-01-19 16:21:04 +0300

    fix: ban icon

--- src/shared/ui/icons/ban/BanIcon.tsx
+++ src/shared/ui/icons/ban/BanIcon.tsx
@@ -1,15 +1,44 @@
 import * as React from 'react'
 import { Ref, SVGProps, forwardRef, memo } from 'react'
+
 const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
-    fill={'none'}
-    height={17}
+    fill={'#ffffff'}
+    height={24}
     ref={ref}
-    width={12}
+    viewBox={'0 0 32.00 32.00'}
+    width={24}
     xmlns={'http://www.w3.org/2000/svg'}
     {...props}
+    stroke={'#ffffff'}
+    strokeWidth={'0.00032'}
   >
-    <path d={'m1.043 16.362 10-15'} stroke={'#fff'} strokeWidth={2.3} />
+    <g id={'SVGRepo_bgCarrier'} strokeWidth={'0'}></g>
+    <g
+      id={'SVGRepo_tracerCarrier'}
+      stroke={'#CCCCCC'}
+      strokeLinecap={'round'}
+      strokeLinejoin={'round'}
+      strokeWidth={'0.064'}
+    ></g>
+    <g id={'SVGRepo_iconCarrier'}>
+      <g data-name={'Group 30'} id={'Group_30'} transform={'translate(-310.001 -321.695)'}>
+        <path
+          d={
+            'M326,321.7a16,16,0,1,0,16,16A16,16,0,0,0,326,321.7Zm0,28a12,12,0,1,1,12-12A12,12,0,0,1,326,349.7Z'
+          }
+          data-name={'Path 364'}
+          id={'Path_364'}
+        ></path>
+        <rect
+          data-name={'Rectangle 41'}
+          height={'4'}
+          id={'Rectangle_41'}
+          transform={'translate(314.348 346.523) rotate(-45.001)'}
+          width={'28.969'}
+        ></rect>
+      </g>
+    </g>
   </svg>
 )
 const ForwardRef = forwardRef(SvgComponent)
