56542cb420e585806c6d8e55512e7cee1bdd2717
sonyhero <27anton06@gmail.com>
2024-01-29 19:20:11 +0300

    feat: add some pages

---
+++ src/pages/admin/posts/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { getBaseLayout } from '@/shared'
+
+const PostsPage = () => {
+  return <div>Posts</div>
+}
+
+export default PostsPage
+PostsPage.getLayout = getBaseLayout
