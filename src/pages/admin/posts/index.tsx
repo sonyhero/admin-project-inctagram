27209e08e022a761c7964d7c9ef686cebfb1847b
sonyhero <27anton06@gmail.com>
2024-01-30 15:06:42 +0300

    feat: get posts

--- src/pages/admin/posts/index.tsx
+++ src/pages/admin/posts/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+import { useGetPostsQuery } from '@/entities/posts-list/postListApi.generated'
 import { getBaseLayout } from '@/shared'
 
 const PostsPage = () => {
+  const { data, error, loading } = useGetPostsQuery({
+    variables: {
+      // endCursorPostId: '', // value for 'endCursorPostId'
+      pageSize: 10,
+      searchTerm: '',
+      sortBy: 'asc',
+      // sortDirection: ''
+    },
+  })
+
+  console.log(data)
+
   return <div>Posts</div>
 }
 
