9a8597506aec3881da69821411c74691370063d6
sonyhero <27anton06@gmail.com>
2024-07-09 19:28:00 +0300

    fix: link icon colors

--- src/shared/ui/icons/payments/PaytmensIcon.tsx
+++ src/shared/ui/icons/payments/PaytmensIcon.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Ref, SVGProps, forwardRef, memo } from 'react'
 const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
-    fill={'none'}
+    fill={props.color}
     height={24}
     ref={ref}
     width={24}
@@ -13,12 +13,8 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
       d={
         'M19 5H5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3ZM4 8a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v1H4V8Zm16 8a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-5h16v5Z'
       }
-      fill={'#fff'}
     />
-    <path
-      d={'M7 15h4a1 1 0 0 0 0-2H7a1 1 0 0 0 0 2ZM15 15h2a1 1 0 0 0 0-2h-2a1 1 0 0 0 0 2Z'}
-      fill={'#fff'}
-    />
+    <path d={'M7 15h4a1 1 0 0 0 0-2H7a1 1 0 0 0 0 2ZM15 15h2a1 1 0 0 0 0-2h-2a1 1 0 0 0 0 2Z'} />
   </svg>
 )
 const ForwardRef = forwardRef(SvgComponent)
